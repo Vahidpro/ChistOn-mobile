09c5d0d662abc9a42676b7b6d93e61e6610b7be9
Vahid Porki <ivahidp@gmail.com>
2023-02-15 11:48:00 +0330

    Styling setting and favorite

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,12 +1,45 @@
-import { StyleSheet, Text } from "react-native";
-
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "../constants/colors";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
 function Favorites() {
-	return <Text style={styles.container}>چیستان های مورد علاقه</Text>;
+	const [fontsLoaded] = useFonts({
+		"Vazirmatn-Regular": require("../assets/fonts/Vazirmatn-Regular.ttf"),
+		"Vazirmatn-Bold": require("../assets/fonts/Vazirmatn-Bold.ttf"),
+	});
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded]);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	return (
+		<View onLayout={onLayoutRootView} style={styles.container}>
+			<Text style={styles.text}>✅ در نسخه های بعدی...</Text>
+		</View>
+	);
 }
 export default Favorites;
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: colors.primary900,
+		color: "white",
+		fontFamily: "Vazirmatn-Regular",
+	},
+	text: {
+		flex: 1,
+		backgroundColor: colors.primary900,
+		marginVertical: 15,
+		marginHorizontal: 24,
+		fontSize: 18,
+		color: "white",
+		fontFamily: "Vazirmatn-Regular",
 	},
 });
