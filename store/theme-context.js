33a6935bed69c63e7ad2f94f9bd3cdc5e0a85e42
Vahid Porki <ivahidp@gmail.com>
2023-03-24 06:01:53 +0330

    cleaned theme-context codes

--- store/theme-context.js
+++ store/theme-context.js
@@ -1,15 +1,6 @@
 import React from "react";
 
 export const PreferencesContext = React.createContext({
-	toggleTheme: () => {
-		const storeData = async (status) => {
-			try {
-				const state = JSON.stringify(status);
-				await AsyncStorage.setItem("@darkthemestate", state);
-			} catch (e) {
-				console.log(e);
-			}
-		};
-	},
+	toggleTheme: () => {},
 	isThemeDark: false,
 });
